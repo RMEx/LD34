6b0ada787c820fca84fa7ec24192f84028e4d8ae
yoann-dufresne <yoann.dufresne0@gmail.com>
2015-12-12 17:20:10 +0100

    protos

--- js/stage.js
+++ js/stage.js
@@ -1,37 +1,39 @@
 Graphics.Parallax =
     function(file, scroll_x, scroll_y, auto_x, auto_y) {
-        
         this.file = file;
         this.scroll_x = scroll_x;
         this.scroll_y = scroll_y;
         this.auto_x = auto_x;
         this.auto_y = auto_y;
-
-        this.asTile = function() {
-            if (this.tile == undefined) {
-                var texture = PIXI.Texture.fromImage(file);
-                this.tile = new PIXI.extras.TilingSprite(
-                    texture,
-                    renderer.width,
-                    renderer.height
-                );
-            }
-            return this.tile;
-        }
-
-        this.render = function(stage) {
-            stage.raw().addChild(this.asTile());
-        }
-
-        
-        
     };
+
+Graphics.Parallax.prototype = {
+    asTile : function() {
+        if (this.tile == undefined) {
+            var texture = PIXI.Texture.fromImage(file);
+            this.tile = new PIXI.extras.TilingSprite(
+                texture,
+                renderer.width,
+                renderer.height
+            );
+        }
+        return this.tile;
+    },
+
+    render : function(stage) {
+        stage.raw().addChild(this.asTile());
+    }
+};
 
 Graphics.Stage = function() {
     this.stage = new PIXI.Container();
     this.parallaxes = [];
-    this.raw = function() { return this.stage; }
-    this.addParallax = function(file, sx, sy, ax, ay) {
+};
+
+Graphics.Stage.prototype = {
+    raw : function() { return this.stage; },
+
+    addParallax : function(file, sx, sy, ax, ay) {
         var parallax = new Graphics.Parallax(file, sx, sy, ax, ay);
         parallax.render(this);
         this.parallaxes.push(parallax)
