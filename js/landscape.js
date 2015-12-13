d45cd3384773940dc40b3bd34219e75fc631595e
Bastien Duplessier <bastien.duplessier@gmail.com>
2015-12-13 19:24:27 +0100

    hitbox for landspace (using SAT)

--- js/landscape.js
+++ js/landscape.js
@@ -1,16 +1,3 @@
 Landscape = function(x, y, points) {
-    // draw a shape
-    var graphics = new PIXI.Graphics();
-    this.graphics = graphics;
-    this.graphics.beginFill(0xFF3300);
-    this.graphics.moveTo(x, y);
-    points.forEach(function(point) {
-	      graphics.lineTo(point[0], point[1]);
-    });
-    this.graphics.endFill();
-    this.graphics.alpha = 0;
-}
-
-Landscape.prototype = {
-    containsPoint: function(point) { return this.graphics.containsPoint(point); }
+    this.hitbox = new SAT.Polygon(new SAT.Vector(x,y), points.map(function(pt) { return new SAT.Vector(pt[0], pt[1]) }));
 }
