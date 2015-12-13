550d65824c3def0e697de7c931f1bfcbf0d2d223
yoann-dufresne <yoann.dufresne0@gmail.com>
2015-12-13 15:18:09 +0100

    Foot noise test

--- js/assets.js
+++ js/assets.js
@@ -1,3 +1,12 @@
 Assets.Audio = {
-    test: new Audio('assets/music/test.mp3')
+    test: new Audio('assets/music/test.mp3'),
+    pas: new Audio('assets/music/pas.mp3'),
+
+    init: function () {
+    	var that = this;
+    	this.pas.addEventListener('ended', function() {
+    		that.pas.currentTime = 0;
+    		that.pas.play();
+		}, false);
+    }
 }
--- js/game.js
+++ js/game.js
@@ -1,6 +1,20 @@
 var stage = new Graphics.Stage();
 stage.addParallax('assets/images/bc.png', 3, 1, 3, 0);
-Assets.Audio.test.play();
+Assets.Audio.init();
+
+// Feet sound test
+Assets.Audio.pas.play();
+/*var play = false;
+window.onclick = function () {
+	if (play) {
+		play = false;
+		Assets.Audio.pas.pause();
+	} else {
+		play = true;
+		Assets.Audio.pas.play();
+	}
+}/**/
+
 animate();
 function animate() {
     
